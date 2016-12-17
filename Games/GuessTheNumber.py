30a6f517e7d2a89e7b9bb0eb43208385119776a8
WalterBrito <waltter.britto@gmail.com>
2016-12-17 10:56:45 -0300

    Python games!

--- Games/GuessTheNumber.py
+++ Games/GuessTheNumber.py
@@ -1,35 +1,52 @@
 # -*- coding: utf-8 -*-
-
-# Guess the number game
-
+# This is a Guess the number game.
 import random
+import text
 
 guessesTaken = 0
-
-myName = input("Hello what's your name?: ").capitalize()
+myName = input("Hello! What's  your name?: ").capitalize()
 
 number = random.randint(1, 20)
-print("Well, " + myName + ", I'm thinking of a number between 1 and 20. ")
+print("Well, " + myName + ", I am thinking of a number between 1 and 20.")
 
 while guessesTaken < 6:
-	guess = int(input("Take a guess: "))
+    print('Take a guess: ') # There are four spaces in front of print
+    guess = int(input('Type a number (1 - 20): '))
+    guessesTaken += 1
 
-	guessesTaken += 1
-
-	if guess < number:
-		print("Your guess is too low.")
-
-	if guess > number:
-		print("Your guess is too high.")
-
-	if guess == number:
-		break
+    if guess < number:
+        print('Your guess is too low.') # There are eight spaces in front of print.
+    if guess > number:
+        print('Your guess is too high.')
+    if guess == number:
+        break
 
 if guess == number:
-	guessesTaken = str(guessesTaken)
-	print("Good job, " + myName + "! You guesses my number in " +
-guessesTaken + " guesses!")
+    guessesTaken = str(guessesTaken)
+    print('Good job, ' + myName + '! You guessed my number in '
+         + guessesTaken + ' guesses!')
 
 if guess != number:
-	number = str(number)
-	print('Nope. The number I was thinking of was ' + number)
+    number = str(number)
+    print('Nope. The number I was thinking of was ' + number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
