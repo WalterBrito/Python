8ca5aa011089bd7221fafae902cf1a90ea5e1e1a
WalterBrito <waltter.britto@gmail.com>
2017-04-28 19:58:34 -0300

    Python

---
+++ SalaryManager/salaryControl.py
@@ -0,0 +1,36 @@
+# Empty dictionary to store expenses and values to being pay
+expensesNameValue = {}
+
+# Get the user salary
+salary = float(input("Enter the salary balance (ex: 1980.58 ): "))
+
+# Loop info about user expanses
+while True:
+    # User enter the expenses and salary
+    expenses = input("Enter the name os the expense (ex: bag): ").capitalize()
+    expensesCharge = float(input("Enter the charge value (ex: 52.90): "))
+    finish = input('Do you wanna exit? (y or n): ').lower()
+
+    # Exit the program when user finished to enter expenses
+    if finish == 'y':
+        break
+
+    if True:
+        expensesNameValue[expenses] = expensesCharge
+    else:
+        print('What your type is not an valid option!\n Try again!')
+
+for key, value in expensesNameValue.items():
+    print('========== Your Expenses ==========\n')
+    print('\t' + "{}: {}".format(key, value))
+    print('=' * 40)
+
+
+
+
+
+
+
+
+
+
